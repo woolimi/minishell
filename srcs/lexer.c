5a273beccf69a5b24777a2b66b0c017eecebb69c
flavienfr <44776722+flavienfr@users.noreply.github.com>
2020-02-14 12:51:13 +0100

    semilicon

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -12,6 +12,57 @@
 
 #include "minishell.h"
 
+int		count_semicolon(char *line)
+{
+	int count;
+
+	count = 0;
+	while(*line)
+	{
+		if(*line == '\'' || *line == '\"')
+		{
+			*line++;
+			while(*line != '\'' && *line != '\"' && *line)
+				*line++;
+			if(*line == '\0')
+				printf("QUOTES NEED TO BE CLOSE\n");
+		}
+		if(*line == ';')
+			count++;
+		*line++;
+	}
+	return(count);
+}
+
+char	**parse_semicolon(char *line)
+{
+	char	**tokens;
+	int		i;
+	int		pos;
+	int		start;
+
+	i = count_semicolon(line);
+	tokens = (char **)malloc(sizeof(*char) * (i + 1));
+	i = 0;
+	pos = 0 ;
+	start = 0;
+	while(line[i])
+	{
+		if(line[i] == '\'' || line[i] == '\"')
+			while(line[i] != '\'' && line[i] != '\"')
+				i++;
+		if(line[i] == ';')
+		{
+			tokens[pos] = ft_substr(line, start, i);
+			pos++;
+			start = i;
+		}
+	}
+	tokens[pos] = ft_substr(line, start, i);
+	tokens[pos + 1] = NULL;
+	return(tokens);
+}
+
 void	lexer(char *line)
 {
 	int i;
