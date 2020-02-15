42a654f208c7afc4df6e9050a94a762c66e51542
flavienfr <44776722+flavienfr@users.noreply.github.com>
2020-02-15 16:01:21 +0100

    lexer is coming

---
+++ srcs/lexer_1.c
@@ -0,0 +1,164 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_1.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: froussel <froussel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/15 15:52:42 by froussel          #+#    #+#             */
+/*   Updated: 2020/02/15 15:58:37 by froussel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+#define SEP_SPACE " \t<>|;"
+#define SEP "<>|;"
+#define SPACE " \t"
+
+static int	count_params(char *line)
+{
+	int i;
+	int count;
+
+	i = 0;
+	count = 0;
+	while (line[i])
+	{
+		if (!ft_haschr(SEP_SPACE, line[i]) && line[i])
+		{
+			count++;
+			while (!ft_haschr(SEP_SPACE, line[i]) && line[i])
+			{
+				i = jmp_quotes(line, i);
+				i++;
+			}
+		}
+		if (ft_haschr(SEP, line[i]))
+			count++;
+		i += check_sep(line, i, 2);
+	}
+	return (count);
+}
+
+static char	*spliter(char *line, int goal)
+{
+	int i;
+	int start;
+	int count;
+
+	i = 0;
+	start = 0;
+	count = 0;
+	while (line[i])
+	{
+		if (!ft_haschr(SEP_SPACE, line[i]) && line[i] && ++count)
+		{
+			while (!ft_haschr(SEP_SPACE, line[i]) && line[i])
+				i = jmp_quotes(line, i) + 1;
+			if (count == goal)
+				return (ft_substr(line, start, i));
+			start = i;
+		}
+		if (ft_haschr(SEP, line[i]) && ++count)
+			if (count == goal)
+				return (ft_substr(line, start, i + check_sep(line, i, 1)));
+		i += check_sep(line, i, 2);
+		start = i;
+	}
+	return (NULL);
+}
+
+static char	*check_token(char *tk, char quote)
+{
+	char	*new_tk;
+	int		size;
+	int		i;
+	int		j;
+
+	i = -1;
+	size = 0;
+	while (tk[++i])
+		if (tk[i] == quote)
+			size++;
+	if (!(new_tk = malloc(sizeof(char) * (ft_strlen(tk) - size + 1))))
+		return (NULL);
+	i = -1;
+	j = -1;
+	while (tk[++i])
+		if (tk[i] != quote)
+			new_tk[++j] = tk[i];
+	new_tk[++j] = '\0';
+	return (new_tk);
+}
+
+static char	**check_tokens(char **tks)
+{
+	char	*new_tk;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (tks[i])
+	{
+		j = -1;
+		while (tks[i][++j])
+		{
+			if (tks[i][j] == '\"' || tks[i][j] == '\'')
+			{
+				new_tk = check_token(tks[i], tks[i][j]);
+				free(tks[i]);
+				tks[i] = new_tk;
+				break ;
+			}
+		}
+		i++;
+	}
+	return (tks);
+}
+
+char		**lexing(char *line)
+{
+	char	**tokens;
+	int		count;
+	int		i;
+
+	i = -1;
+	count = count_params(line);
+	if (!(tokens = (char **)malloc(sizeof(char *) * (count + 1))))
+		printf("ERREUR DE MALLOC");
+	while (++i < count)
+		tokens[i] = spliter(line, i + 1);
+	tokens[i] = NULL;
+	tokens = check_tokens(tokens);
+	return (tokens);
+}
+
+/*
+int main()
+{
+
+	char **tk;
+	int i = 0;
+	//tk = lexing("file>tata");
+	//tk = lexing("cat salut\";o;g; ;s;h;i;t\" tout; le monde je ;vais");
+	//tk = lexing("echo dsf\"fds\"fds 	");
+	//tk = lexing("echo \"fsd   fds\" sfd 	; echo lol");
+	//tk = lexing("echo>>nana");
+	//tk = lexing("file>tata");
+	//tk = lexing("   file>>tata;  ");
+	//tk = lexing("> putain");
+	//tk = lexing("");
+	//tk = lexing("echo \"couc\"o\"\"");
+	//tk = lexing("echo \"'couc'\"o\"'\"");
+	//tk = lexing("e\"\"\"\"\"cho\" salut");
+	tk = lexing("ifconfig -a | grep ether | sed \"s/ether //g\" | sed \"s/  //g\" | sed \"s/ //g\"");
+	while(tk[i])
+	{
+		printf("i=%d (%s)\n", i, tk[i]);
+		free(tk[i]);
+		i++;
+	}
+	free(tk);
+}
+*/
---
+++ srcs/lexer_2.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: froussel <froussel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/15 15:52:34 by froussel          #+#    #+#             */
+/*   Updated: 2020/02/15 15:58:37 by froussel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+int		jump_space(char *line, int i, int jmp)
+{
+	i += jmp;
+	while (line[i] == ' ' || line[i] == '\t')
+	{
+		i++;
+		jmp++;
+	}
+	return (jmp);
+}
+
+int		check_sep(char *line, int i, int use)
+{
+	int jmp;
+
+	if (line[i] == ' ' || line[i] == '\t')
+		jmp = 1;
+	else if (line[i] == '>' && line[i + 1] == '>')
+		jmp = 2;
+	else if (line[i] == '>')
+		jmp = 1;
+	else if (line[i] == '<')
+		jmp = 1;
+	else if (line[i] == '|')
+		jmp = 1;
+	else if (line[i] == ';')
+		jmp = 1;
+	else
+		return (0);
+	if (use == 1)
+		return (jmp);
+	else if (use == 2)
+		return (jump_space(line, i, jmp));
+	return (0);
+}
+
+int		jmp_quotes(char *line, int i)
+{
+	if (line[i] == '\"')
+	{
+		i++;
+		while (line[i] != '\"' && line[i])
+			i++;
+		if (line[i] == '\0')
+			printf("QUOTES NEED TO BE CLOSE\n");
+	}
+	else if (line[i] == '\'')
+	{
+		i++;
+		while (line[i] != '\'' && line[i])
+			i++;
+		if (line[i] == '\0')
+			printf("SINGLE QUOTE NEED TO BE CLOSE\n");
+	}
+	return (i);
+}
