850a6d38d97edba6d5a593d342dbbfd738d0776f
froussel <froussel@student.42.fr>
2020-03-05 15:05:28 +0100

    norme

--- srcs/free_2.c
+++ srcs/free_2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free_2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wpark <wpark@student.42.fr>                +#+  +:+       +#+        */
+/*   By: froussel <froussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 16:36:55 by wpark             #+#    #+#             */
-/*   Updated: 2020/02/24 19:38:40 by wpark            ###   ########.fr       */
+/*   Updated: 2020/03/05 14:28:22 by froussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void
 	t_env *env;
 	t_env *tmp;
 
-	env = get_minish()->env;		
+	env = get_minish()->env;
 	while (env)
 	{
 		tmp = env->next;
